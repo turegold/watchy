5ea5176e741f1a3bf54f91650583c2fbb281e3cb
이용민 <bacaren21@naver.com>
2026-02-07 02:26:59 +0900

    first commit

---
+++ src/main/java/com/watchparty/watchparty/WatchpartyApplication.java
@@ -0,0 +1,13 @@
+package com.watchparty.watchparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchpartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchpartyApplication.class, args);
+	}
+
+}
